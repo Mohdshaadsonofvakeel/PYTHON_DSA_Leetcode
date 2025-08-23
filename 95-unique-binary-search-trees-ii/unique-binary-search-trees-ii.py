1c24952ab1d7c9a2941552e7b6d9bf75adbe20a5
Mohd shad <98746318+Mohdshaadsonofvakeel@users.noreply.github.com>
2025-08-23 16:18:14 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (71.79%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -1,3 +1,6 @@
+from typing import List, Optional
+from functools import lru_cache
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,25 +8,21 @@
 #         self.left = left
 #         self.right = right
 
-from typing import List, Optional
-
 class Solution:
     def generateTrees(self, n: int) -> List[Optional['TreeNode']]:
         if n == 0:
             return []
 
+        @lru_cache(None)  # Cache results for (start, end)
         def build(start: int, end: int) -> List[Optional['TreeNode']]:
             if start > end:
                 return [None]
 
             all_trees = []
             for root_val in range(start, end + 1):
-                # All possible left subtrees
                 left_trees = build(start, root_val - 1)
-                # All possible right subtrees
                 right_trees = build(root_val + 1, end)
 
-                # Combine left and right subtrees with the root
                 for l in left_trees:
                     for r in right_trees:
                         root = TreeNode(root_val)
